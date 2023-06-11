bbb96243f6bdf77f99574062416acaba483020c3
Austin Smith <asmit2222@gmail.com>
2023-06-11 18:15:54 -0400

    nm

--- src/Days/Sunday.tsx
+++ src/Days/Sunday.tsx
@@ -1,7 +1,29 @@
 import React, { useState, useRef, TouchEvent, useEffect } from "react";
 
 const Sunday: React.FC = () => {
-  return <div>hi</div>;
+  return (
+    <div className="dayContainer">
+      <div className="dayTitle">Sunday</div>
+      <ul>
+        <li>
+          Category 2
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+          </ul>
+        </li>
+        <li>
+          Category 3
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sunday;
